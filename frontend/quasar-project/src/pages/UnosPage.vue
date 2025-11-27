5eebf8286d7e28de7b289173c8d9fc92ef8357bc
akozul <anakozul556@gmail.com>
2025-11-27 13:14:01 +0100

    kolokvij

---
+++ frontend/quasar-project/src/pages/UnosPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="text-h4">Ovo su gumbi</h1>
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn push color="primary" label="Push" />
+    <q-btn push color="primary" round icon="card_giftcard" />
+    <br>
+    <q-btn push color="white" text-color="primary" label="Push" />
+    <q-btn push color="white" text-color="primary" round icon="card_giftcard" />
+  </div>
+</template>
